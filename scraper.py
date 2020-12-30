d9990c84015ade171bd1990abdbe2b06836593f2
Lydia Vandenbergh Jackson <45639445+lvanden@users.noreply.github.com>
2020-12-29 21:47:40 -0500

    Add DMARC lookup

--- scraper.py
+++ scraper.py
@@ -1,6 +1,9 @@
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
+import dns.resolver 
+import tldextract
+
 
 url = 'https://www.zyxware.com/articles/4344/list-of-fortune-500-companies-and-their-websites'
 page = requests.get(url)
@@ -13,7 +16,7 @@ data_table = soup_object.find_all('table', 'data-table')[0]
 # get all row elements
 all_values = data_table.find_all('tr')
 
-fortune_500_df = pd.DataFrame(columns = ['rank', 'company', 'website'])
+fortune_500_df = pd.DataFrame(columns = ['rank', 'company', 'website', 'domain', 'dmarc_record'])
 index = 0
 
 for row in all_values[1:]:
@@ -23,9 +26,18 @@ for row in all_values[1:]:
     rank = values[0].text
     company = values[1].text
     website = values[2].text
+    # Extract the domain
+    ext = tldextract.extract(website)
+    domain = ext.registered_domain
+    # Get the DMARC record
+    try:
+        txt_records = dns.resolver.resolve('_dmarc.' + domain, 'TXT')
+        for record in txt_records:
+            dmarc_record = record.to_text()
+    except Exception as e:
+        dmarc_record = ''
 
-    fortune_500_df.loc[index] = [rank, company, website]
+    fortune_500_df.loc[index] = [rank, company, website, domain, dmarc_record]
     index += 1
 
-# get DMARC records
 fortune_500_df.to_csv('fortune_500_companies.csv', index=False)
